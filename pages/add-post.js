f11e96f2d7cc659192f825eff874a38803e5a41f
DARRAGH COEN - STUDENT <G00370083@atu.ie>
2022-11-02 14:05:20 +0000

    Initial commit

---
+++ pages/add-post.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+import Nav from '../components/Nav';
+import styles from '../styles/Home.module.css';
+
+async function addPost(req, res) {
+    try {
+        // connect to the database
+        let { db } = await connectToDatabase();
+        // add the post
+        await db.collection('posts').insertOne(JSON.parse(req.body));
+        // return a message
+        return res.json({
+            message: 'Post added successfully',
+            success: true,
+        });
+    } catch (error) {
+        // return an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+}
